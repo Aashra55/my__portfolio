515933deef3e50619f648edd600f05aaf7b9ab6a
Aashra <saleemlatif17@gmail.com>
2024-11-12 23:47:57 +0500

    update links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -44,12 +44,12 @@ export default function Home() {
         lg:w-[70%] w-[95%] md:w-[94%] leading-relaxed font-light lg:text-[14px] text-[11px]">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptate quisquam temporibus ducimus sunt non consequuntur, molestiae fugit rem omnis ut porro odit.</p>
         <ul className="transform transition-all translate-y-full opacity-0 duration-1000 initial-position
         flex justify-left gap-[10px] items-center mt-[12px] lg:w-[55%] w-[95%] md:w-[100%]">
-          <li className="lg:p-[10px] p-[8px] lg:pr-[20px] lg:pl-[20px] border-yellow-300 border-2 rounded-2xl lg:mr-[14px] lg:w-[180px] w-[140px] text-[14px] lg:text-[17px]  md:pl-[10px] md:pr-[10px] md:w-[160px] md:ml-[10px] text-center
-          hover:bg-yellow-300 hover:text-black transition-color duration-300 cursor-pointer">Download CV <FontAwesomeIcon icon={faArrowDown}></FontAwesomeIcon></li>
-          <Link href={''}><li className="lg:text-xl text-sm w-[26px] border-yellow-300 border-2 rounded-full text-yellow-300 bg-black shadow-lg hover:shadow-[0_0_10px_4px_rgba(252,211,77,0.7)] transition-color duration-300
+          <Link href={'./mycv.pdf'} download={'mycv'}><li className="lg:p-[10px] p-[8px] lg:pr-[20px] lg:pl-[20px] border-yellow-300 border-2 rounded-2xl lg:mr-[14px] lg:w-[180px] w-[140px] text-[14px] lg:text-[17px]  md:pl-[10px] md:pr-[10px] md:w-[160px] md:ml-[10px] text-center
+          hover:bg-yellow-300 hover:text-black transition-color duration-300 cursor-pointer">Download CV <FontAwesomeIcon icon={faArrowDown}></FontAwesomeIcon></li></Link>
+          <Link href={'https://github.com/account'}><li className="lg:text-xl text-sm w-[26px] border-yellow-300 border-2 rounded-full text-yellow-300 bg-black shadow-lg hover:shadow-[0_0_10px_4px_rgba(252,211,77,0.7)] transition-color duration-300
            lg:w-[34px] lg:mr-[8px] text-center p-[2px] hover:text-black hover:bg-yellow-300">
           <FontAwesomeIcon icon={faGithub} /></li></Link>
-          <Link href={''}><li className="lg:text-xl text-sm w-[26px] border-yellow-300 border-2 rounded-full text-yellow-300 bg-black shadow-lg hover:shadow-[0_0_10px_4px_rgba(252,211,77,0.7)] transition-color duration-300
+          <Link href={'https://www.linkedin.com/in/ashra-saleem-7047a0315?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app'}><li className="lg:text-xl text-sm w-[26px] border-yellow-300 border-2 rounded-full text-yellow-300 bg-black shadow-lg hover:shadow-[0_0_10px_4px_rgba(252,211,77,0.7)] transition-color duration-300
            lg:w-[34px] lg:mr-[8px] text-center p-[2px] hover:text-black hover:bg-yellow-300">
           <FontAwesomeIcon icon={faLinkedinIn} /></li></Link>
           <Link href={''}><li className="lg:text-xl text-sm w-[26px] border-yellow-300 border-2 rounded-full text-yellow-300 bg-black shadow-lg hover:shadow-[0_0_10px_4px_rgba(252,211,77,0.7)] transition-color duration-300
